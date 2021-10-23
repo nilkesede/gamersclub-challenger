3967fa9420fe3586985b7e6eb9c882d47010100a
Carlos Henrique <carloshpds@gmail.com>
2021-10-23 18:50:19 -0300

    environment issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types' : 'off',
     'no-inner-declarations': 'off'
   },
   overrides: [
