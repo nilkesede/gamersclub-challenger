b4bf1d7c2c6c16609086c304de69875bfa6cb3c0
carloshpds <carlos.praeiros@mercadolivre.com>
2023-03-01 22:42:42 -0300

    Create GCCLogo.vue

---
+++ src/components/GCCLogo.vue
@@ -0,0 +1,45 @@
+<template>
+  <span class="gcc-logo-component" :style="logoStyle" title="GamersClub Challenger">
+  </span>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+const GCCLogoComponent = defineComponent({
+  props: {
+    level: {
+      type: Number,
+      required: true
+    }
+  },
+
+  setup(props) {
+    return {
+    };
+  },
+
+  computed: {
+    logoStyle() {
+      return {
+        'background-image': `url(${window.browser.runtime.getURL("../../assets/logo_500.png")})`,
+        'background-repeat': 'no-repeat'
+      }
+    }
+  }
+});
+
+export default GCCLogoComponent
+</script>
+
+<style lang="scss">
+@use "sass:color";
+@import "../styles/_variables.scss";
+
+.gcc-logo-component {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  background-size: cover;
+}
+</style>
