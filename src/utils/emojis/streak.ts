82f3f0e307fb648be997297ecebef1c3baf15dcd
carloshpds <carlos.praeiros@mercadolivre.com>
2023-03-07 20:46:34 -0300

    WIP for streak emojis

---
+++ src/utils/emojis/streak.ts
@@ -0,0 +1,20 @@
+
+const knownWinEmojisMap = {
+  1: '😀',
+  2: '😄',
+  3: '',
+  4: '',
+}
+
+const knownLostEmojisMap = {
+  1: '😒',
+  2: '',
+  3: '',
+  4: '',
+}
+
+
+
+const getWinStreakEmoji = (streak: number) => {
+
+}
