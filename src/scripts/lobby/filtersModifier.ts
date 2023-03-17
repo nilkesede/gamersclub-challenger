a8c8fb913a617b6dbb881d112b5fe77c9c0355ce
Carlos Henrique <carloshpds@gmail.com>
2023-03-17 02:23:52 -0300

    New GCCFiltersBar Component

--- src/scripts/lobby/filtersModifier.ts
+++ src/scripts/lobby/filtersModifier.ts
@@ -1,12 +1,7 @@
 import $ from 'jquery'
 import { gcSelectors } from '../../utils/gcSelectors'
-import KDRFilterComponent from '../../components/KDRFilter.vue'
-import LobbyNameFilterComponent from '../../components/LobbyNameFilter.vue'
 import { createApp } from 'vue'
-import { cleanSelector } from '@/utils/StringUtils'
-import gcBooster from '@/utils/gcBooster'
-import lobbyFilter from './lobbyFilter'
-import BrowserStorage from '../../utils/storage'
+import GCCFiltersBar from '../../components/GCCFiltersBar.vue'
 
 export default class FiltersModifier {
 
@@ -15,39 +10,21 @@ export default class FiltersModifier {
   }
 
   modifyFilters() {
-    this.insertKDRFilter()
-    this.insertLobbyNameFilter()
+    this.insertFiltersBar()
   }
 
-  insertLobbyNameFilter() {
-    if (BrowserStorage.settings.options?.enableNameFilter) {
-      const $roomsContent = $(gcSelectors.lobbies.content)
-      const containerName = `gcc-lobby-player-filter-container`
-      const $container = `<div id='${containerName}' class='${cleanSelector(gcSelectors.extension.appContainer)}'></div>`
-      const $lobbyFilter = $roomsContent.find(containerName)
+  insertFiltersBar(){
+    const $filtersContainer = $(gcSelectors.lobbies.filters.container)
+    const containerId = `gcc-filters-bar-container`
+    const $filtersBar = $filtersContainer.find(containerId)
+    const $container = $('<div>', {
+      id: containerId,
+      class: gcSelectors.extension.appContainer.cleanCSSSelector()
+    })
 
-      if ($lobbyFilter.length === 0) {
-        $roomsContent.prepend($container)
-        createApp(LobbyNameFilterComponent).mount(`#${containerName}`)
-      }
-    }
-  }
-
-  insertKDRFilter() {
-    if (BrowserStorage.settings.options?.enableKDRFilter) {
-      const $filtersContainer = $(gcSelectors.lobbies.filters.container)
-      const $content = $filtersContainer.find(gcSelectors.lobbies.filters.content)
-      const sections = $content.find(gcSelectors.lobbies.filters.section.self)
-      const sectionModel = sections.get(0)
-      const $gccFilterSection = $(sectionModel!).clone()
-      const cleanContainerClass = cleanSelector(gcSelectors.extension.kdrFilterContainer)
-
-      gcBooster.isInstalled().catch(() => { })
-        .finally(() => {
-          $gccFilterSection.addClass(cleanContainerClass)
-          $content.get(0)!.append($gccFilterSection[0])
-          createApp(KDRFilterComponent, { value: lobbyFilter.filters.kdr || BrowserStorage.defaultSettings.filters?.kdr }).mount($gccFilterSection.get(0)!)
-        })
+    if ($filtersBar.length === 0) {
+      $filtersContainer.append($container)
+      createApp(GCCFiltersBar).mount(`#${containerId}`)
     }
   }
 }
